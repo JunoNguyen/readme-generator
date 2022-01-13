d78c27638914df7db09834971e7a367b5b2b909f
Juno <junonguyen27@gmail.com>
2022-01-13 17:47:22 -0600

    made prompts using inquirer

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the Title of your Project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a Description of your Project',
+        name: 'desc',
+    },
+    {
+        type: 'input',
+        message: 'Explain how to use your code.',
+        name: 'explain',
+    }
+];
+
+const inputInfo = inquirer 
+    .prompt(questions)
+    .then((response) => {
+        console.log(response);
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
